d4577ce2f76dad5085acf106267de73c3fcf2068
Steven <zhangc2@illinois.edu>
2023-12-14 14:33:42 -0600

    updated openai api

--- app/page.js
+++ app/page.js
@@ -12,35 +12,36 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import coverImage from "../public/SSurfCover.png";
 
+import { Box, Heading, Text } from "@chakra-ui/react";
+import { FunctionPage } from "twilio/lib/rest/serverless/v1/service/function";
+
 export default function Home() {
   return (
-    <body className={styles.body}>
-      <ChakraProvider className={styles.ChakraProvider}>
-        <header>
-          <div className={styles.imageContainer}>
-            <Image
-              src={coverImage}
-              className={styles.coverImage}
-              width={200}
-              height={200}
-              placeholder="blur"
-              alt="Website Logo"
-              priority
-            />
-          </div>
-          <h1 className={styles.title}>Secure Surf</h1>
-          <p className={styles.description}>
-            Please enter a domain name to get a Secure Surf Score.
-          </p>
-        </header>
-        <div className={styles.content}>
-          <Head>
-            <title>Secure Surf</title>
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-          <Search />
+    <ChakraProvider>
+      <Head>
+        <title>Secure Surf</title>
+        <meta name="Secure Surf - Web safety!" content="Enter a domain name to get a Secure Surf Score." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <div className={styles.imageContainer}>
+          <Image
+            src={coverImage}
+            alt="Website Logo"
+            width={200}
+            height={200}
+            placeholder="blur"
+            className={styles.image}
+          />
         </div>
-      </ChakraProvider>
-    </body>
+        <Heading as="h1" className={styles.title}>Secure Surf</Heading>
+      </header>
+      <main>
+        <Text className={styles.description}>
+          Please enter a domain name to get a Secure Surf Score.
+        </Text>
+        <Search />
+      </main>
+    </ChakraProvider>
   );
 }
