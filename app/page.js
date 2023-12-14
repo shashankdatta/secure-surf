4952c78640740b038fbb56f17f3c387d6668a90e
Steven <zhangc2@illinois.edu>
2023-12-14 14:50:27 -0600

    refractor page, improved ui, updated nextjs

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 "use client";
 
-// next 13
-// beta.nextjs.org
-// lot of improvements since next 12
-// but 13 still has a bunch of bugs...
-
 import Head from "next/head";
 import Search from "../assets/components/search/Search";
 import { ChakraProvider, Container } from "@chakra-ui/react";
@@ -15,33 +10,35 @@ import coverImage from "../public/SSurfCover.png";
 import { Box, Heading, Text } from "@chakra-ui/react";
 import { FunctionPage } from "twilio/lib/rest/serverless/v1/service/function";
 
+const Header = () => (
+  <Box as="header" className={styles.header}>
+    <Image
+      src={coverImage}
+      alt="Website Logo"
+      width={200}
+      height={200}
+      placeholder="blur"
+    />
+    <Heading as="h1" className={styles.title}>Secure Surf</Heading>
+  </Box>
+);
+
 export default function Home() {
   return (
     <ChakraProvider>
       <Head>
         <title>Secure Surf</title>
-        <meta name="Secure Surf - Web safety!" content="Enter a domain name to get a Secure Surf Score." />
+        <meta name="description" content="Enter a domain name to get a Secure Surf Score." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header>
-        <div className={styles.imageContainer}>
-          <Image
-            src={coverImage}
-            alt="Website Logo"
-            width={200}
-            height={200}
-            placeholder="blur"
-            className={styles.image}
-          />
-        </div>
-        <Heading as="h1" className={styles.title}>Secure Surf</Heading>
-      </header>
-      <main>
+      <Header />
+      <Box as="main" className={styles.main}>
         <Text className={styles.description}>
           Please enter a domain name to get a Secure Surf Score.
         </Text>
         <Search />
-      </main>
+      </Box>
     </ChakraProvider>
   );
 }
